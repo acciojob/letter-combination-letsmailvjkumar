a1a1d55c583bfb09c200d0834ea5d8c17a2fab56
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-26 17:14:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	const digitMap = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+  const combinations = [];
+
+  function backtrack(current, nextDigits) {
+    if (nextDigits.length === 0) {
+      // No more digits to process, add current combination
+      combinations.push(current);
+    } else {
+      const digit = nextDigits[0];
+      const letters = digitMap[digit];
+
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(current + letter, nextDigits.slice(1));
+      }
+    }
+  }
+
+  if (digits) {
+    backtrack('', digits);
+  }
+
+  return combinations;
 }
 
 module.exports = letterCombinations;
